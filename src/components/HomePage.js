956cdb2d8a832c46716a043aef11eb21da50353f
raezon <amardjebabla10@gmail.com>
2023-03-07 13:25:24 +0100

    Enhance prototype

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -48,7 +48,11 @@ function HomePage() {
       return;
     }
 
-    const res = await axios.post('https://api.gutesknie.de/api/stripe/pay', {email: email,amount:523323,currency: 'usd'});
+    const res = await axios.post(`${process.env.BACKEND_URL}/pay`, {
+      email: email,
+      amount: 523323,
+      currency: 'usd',
+    });
 
     const clientSecret = res.data['client_secret'];
 
@@ -95,7 +99,7 @@ function HomePage() {
     if (result.error) {
       console.log(result.error.message);
     } else {
-      const res = await axios.post('https://api.gutesknie.de/api/stripe/pay', {'payment_method': result.paymentMethod.id, 'email': email});
+      const res = await axios.post(`${process.env.BACKEND_URL}/pay`, {'payment_method': result.paymentMethod.id, 'email': email});
       // eslint-disable-next-line camelcase
       const {client_secret, status} = res.data;
 
